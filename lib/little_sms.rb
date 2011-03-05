edcd91d9deeec35dacdecff9154b01c95395e6d2
TweeKane <tweekane@gmail.com>
2011-03-05 12:08:35 +0300

    Rename LittleSMS class file

--- lib/little_sms.rb
+++ lib/little_sms.rb
@@ -2,7 +2,7 @@ libdir = File.dirname(__FILE__)
 $LOAD_PATH.unshift(libdir) unless $LOAD_PATH.include?(libdir)
 
 require "#{libdir}/little_sms/component"
-require "#{libdir}/little_sms/littlesms"
+require "#{libdir}/little_sms/little_sms"
 
 class LittleSMS; end
 
